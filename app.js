94fe1ec9d8bc455f935cca67a50b86bf471d7ff2
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-12-12 20:07:20 +0400

    Arrays & Tupples Testout

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+const coords = [
+    [1, []],
+    [-1, []]
+];
+console.log("coords:", typeof (coords), coords[0][0]);
---
+++ app.ts
@@ -0,0 +1,6 @@
+const coords: [number, number[]][] =[
+    [1,[]],
+    [-1,[]]
+]
+ 
+console.log("coords:",coords[0][0])
