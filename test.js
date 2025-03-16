f839d9ea93a48a406e2c87653c984a898bc87fa2
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-16 15:50:23 +0400

    functions

--- test.js
+++ test.js
@@ -1,5 +1,14 @@
 "use strict";
 function makeName(firstName, lastName, middleName) {
+    if (middleName)
+        return firstName + " " + middleName + " " + lastName;
+    else
+        return firstName + " " + lastName;
 }
-const fullName = makeName("Misho", "Silagava");
-console.log(fullName);
+// const fullName = makeName("Misho", "Silagava")
+// console.log(fullName)
+function callFunc(func, param1, param2) {
+    return func(param1, param2);
+}
+const result = callFunc(makeName, "Misho", "Silagava");
+console.log(result);
--- test.ts
+++ test.ts
@@ -3,6 +3,18 @@ function makeName(firstName: string, lastName: string, middleName?: string) {
     else return firstName + " " + lastName
 }
 
-const fullName = makeName("Misho", "Silagava")
+// const fullName = makeName("Misho", "Silagava")
 
-console.log(fullName)
+// console.log(fullName)
+
+function callFunc(
+    func: (f:string, l:string, m?:string) => string,
+    param1: string,
+    param2: string
+) {
+    return func(param1, param2)
+}
+
+const result = callFunc(makeName, "Misho", "Silagava")
+
+console.log(result)
